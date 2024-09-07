ffeaee77d88dbffb6b95d2c30d2c427ad6c2189b
tomoko3535 <neko00814@gmail.com>
2024-09-07 11:12:02 +0900

    初回コミット

--- Student.Management/src/main/java/raisetech/Student/Management/Application.java
+++ Student.Management/src/main/java/raisetech/Student/Management/Application.java
@@ -1,16 +1,30 @@
 package raisetech.Student.Management;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	public static void main(String[] args) {
+	private String name = "Enami Kouji";
+    public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping("/name")
+	public String getName(){
+		return name;
+	}
+	@PostMapping("/name")
+	public void setName(String name){
+		this.name = name;
+	}
 }
--- Student.Management/src/main/java/raisetech/Student/Management/ServletInitializer.java
+++ Student.Management/src/main/java/raisetech/Student/Management/ServletInitializer.java
@@ -1,13 +1,15 @@
 package raisetech.Student.Management;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import lombok.Getter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
- @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-	 return application.sources(Application.class);
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
