f07915f5c05bc6c10ec6ba568043dba2aa32c72b
tomoko3535 <neko00814@gmail.com>
2024-09-20 17:32:40 +0900

    初回コミット

--- Student.Management/src/main/java/raisetech/Student/Management/Application.java
+++ Student.Management/src/main/java/raisetech/Student/Management/Application.java
@@ -3,10 +3,9 @@ package raisetech.Student.Management;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -16,27 +15,17 @@ public class Application {
 	@Autowired
 	private StudentRepository repository;
 
-	private String name = "Yamada Taro";
-	private String age = "30";
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
 	}
-	@GetMapping("/student")
-	public String getStudent(){
-		Student student = repository.searchByName("YoshidaHanako");
-		return student.getName() + " " + student.getAge() + "歳";
-	}
 
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-
-	}
-
-	@PostMapping("/studentName")
-	public void updateStudentName(String name){
-		this.name = name;
+	@GetMapping("/students_coursesList")
+	public List<Student> getStudentcoursesList() {
+		return repository.search();
 	}
 }
+
+
 
