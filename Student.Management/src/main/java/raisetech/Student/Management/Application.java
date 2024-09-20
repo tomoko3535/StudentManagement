fa0539d3fee5e89a9bc9e35f4fd9f8079a5634fb
tomoko3535 <neko00814@gmail.com>
2024-09-20 17:32:40 +0900

    初回コミット

--- Student.Management/src/main/java/raisetech/Student/Management/Application.java
+++ Student.Management/src/main/java/raisetech/Student/Management/Application.java
@@ -3,10 +3,7 @@ package raisetech.Student.Management;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -16,27 +13,32 @@ public class Application {
 	@Autowired
 	private StudentRepository repository;
 
-	private String name = "Yamada Taro";
-	private String age = "30";
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 	@GetMapping("/student")
-	public String getStudent(){
-		Student student = repository.searchByName("YoshidaHanako");
+	public String getStudent(@RequestParam String name) {
+		Student student = repository.searchByName(name);
 		return student.getName() + " " + student.getAge() + "歳";
 	}
 
 	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-
+	public void registerStudent(String name, int age) {
+		repository.registerStudent(name,age);
 	}
 
-	@PostMapping("/studentName")
-	public void updateStudentName(String name){
-		this.name = name;
+	@PatchMapping("/student")
+	public void updateStudent(String name,int age) {
+		repository.updateStudent(name, age);
+	}
+	@DeleteMapping("/student")
+	public void deleteStudent(String name){
+		repository.deleteStudent(name);
 	}
 }
+
 
--- Student.Management/src/main/java/raisetech/Student/Management/StudentRepository.java
+++ Student.Management/src/main/java/raisetech/Student/Management/StudentRepository.java
@@ -1,8 +1,6 @@
 package raisetech.Student.Management;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 
@@ -13,4 +11,10 @@ public interface StudentRepository {
 
     @Insert("INSERT student values(#{name}, #{age})")
     void registerStudent(String name, int age);
+
+    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+    void updateStudent(String name,int age);
+
+    @Delete("DELETE FROM student WHERE name = #{name}")
+    void deleteStudent(String name);
 }
