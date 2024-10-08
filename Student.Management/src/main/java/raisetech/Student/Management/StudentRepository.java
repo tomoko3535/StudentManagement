b1e1965c60c68c42e85b092394a9cfb46c17154d
tomo08 <neko00814@gmail.com>
2024-10-08 16:56:55 +0900

    Update StudentRepository.java

--- Student.Management/src/main/java/raisetech/Student/Management/StudentRepository.java
+++ Student.Management/src/main/java/raisetech/Student/Management/StudentRepository.java
@@ -6,15 +6,6 @@ import org.apache.ibatis.annotations.*;
 
 public interface StudentRepository {
 
-    @Select("SELECT * FROM student WHERE name = #{name}")
-    Student searchByName(String name);
-
-    @Insert("INSERT student values(#{name}, #{age})")
-    void registerStudent(String name, int age);
-
-    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-    void updateStudent(String name,int age);
-
-    @Delete("DELETE FROM student WHERE name = #{name}")
-    void deleteStudent(String name);
+    @Select("SELECT * FROM students_courses")
+    List<studentsCourses> search();
 }
