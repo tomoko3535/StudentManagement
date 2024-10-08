ce508ef2aad5ad800b6abc509baeda0731f7cb03
tomo08 <neko00814@gmail.com>
2024-10-08 17:44:56 +0900

    Update Student.java

--- Student.Management/src/main/java/raisetech/Student/Management/Student.java
+++ Student.Management/src/main/java/raisetech/Student/Management/Student.java
@@ -1,23 +1,16 @@
 package raisetech.Student.Management;
 
 public class Student {
+   package raisetech.Student.Management;
+
+public class Student {
+
+    private String id;
     private String name;
+    private String kanaName;
+    private String nickName;
+    private String email;
+    private String area;
     private int age;
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+    private String sex;
 }
