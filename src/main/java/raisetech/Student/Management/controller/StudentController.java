3570e2a73b467158e668cafa4375eb2093b0a96e
tomoko3535 <neko00814@gmail.com>
2025-03-26 14:36:13 +0900

    初回コミット

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -1,8 +1,8 @@
 package raisetech.Student.Management.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
